39f3386d0423be052d09c975a76073f20a211e49
Latti31 <111042692+Latti31@users.noreply.github.com>
2023-06-02 01:23:34 +1000

    Added email link functionality

--- UI.js
+++ UI.js
@@ -1,4 +1,4 @@
-const SETTINGS_WIDTH = 500;
+const SETTINGS_WIDTH = 1500;
 
 const itemText = document.getElementById("itemText");
 const quantityText = document.getElementById("quantityText");
@@ -7,7 +7,7 @@ const clearButton = document.getElementById("clear");
 const addButton = document.getElementById("add");
 const settingsButton = document.getElementById("settings");
 const settingsHolder = document.getElementById("settings-holder");
-const downloadButton = document.getElementById("downloadText");
+// const downloadButton = document.getElementById("downloadText");
 const fScreenButton = document.getElementById("fscreen");
 
 var total = 0;
@@ -19,11 +19,19 @@ var itemElements = []
 
 var panelOpen = false;
 
+let uniqueID = 0;
+
+function getID() {
+    uniqueID += 1;
+    return uniqueID;
+}
+
 class Item {
     constructor(description, quantity, price) {
         this.description = description;
         this.quantity = quantity;
         this.price = price;
+        this.id = getID();
     }
 }
 
@@ -102,10 +110,18 @@ function updateSum() {
     document.getElementById("sumText").innerHTML = "$ " + roundedNumber
 }
 
-function removeElement(element, item) {
-    total -= item.price;
-    updateSum();
-    element.remove();
+function removeElement(element, newID, item) {
+
+    // Will be null if we are using the hacky dummy element
+    if (element) {
+        element.remove();
+        total -= item.price;
+        updateSum();
+    }
+
+    items = items.filter(function (item) {
+        return item.id != newID;
+    });
 }
 
 let waitBeforeCreate = true;
@@ -129,6 +145,7 @@ function createItem() {
     
         const newP = document.createElement('div');
         newP.className = "invoiceItem"
+        newP.id = newItem.id;
 
         const newDesc = document.createElement('p');
         const newQuantity = document.createElement('p');
@@ -163,10 +180,11 @@ function createItem() {
         itemText.value = ""
         quantityText.value = ""
         priceText.value = ""
-        document.getElementById("add2down").innerHTML = ""
+        document.getElementById("add2down").innerHTML = "33"
+        document.getElementById("add2down").style.display = 'none';
 
         newDeleteButton.addEventListener("click", function () {
-            removeElement(newP, newItem);
+            removeElement(newP, newP.id, newItem);
         })
 
         const elements = [newDesc, newQuantity, newPrice];
@@ -174,7 +192,6 @@ function createItem() {
         let maxHeight = 0;
         elements.forEach((element) => {
             const height = element.clientHeight;
-            console.log(height)
           if (height > maxHeight) {
             maxHeight = height;
           }
@@ -193,6 +210,7 @@ function open()
 
     if (items.length == 0) {
         document.getElementById("add2down").innerHTML = "No items added"
+        document.getElementById("add2down").style.display = 'block';
     }
     
     let screenWidth = SETTINGS_WIDTH;
@@ -205,21 +223,21 @@ function open()
     }
     
     document.getElementById("settings").style.color = 'rgb(249, 232, 221)';
-    document.getElementById("downloadText").style.color = defaultColor;
-    document.getElementById("add2down").style.color = defaultColor;
+    // document.getElementById("downloadText").style.color = defaultColor;
+    // document.getElementById("add2down").style.color = defaultColor;
 }
 
-var defaultColor = document.getElementById("downloadText").style.color;
-document.getElementById("downloadText").style = "color: #00000000;"
-document.getElementById("add2down").style = "color: #00000000;"
+// var defaultColor = document.getElementById("downloadText").style.color;
+// document.getElementById("downloadText").style = "color: #00000000;"
+document.getElementById("add2down").style.color = 'white';
 
 function close() {
     document.getElementById("fscreen").style = 'pointer-events: none;';
     document.getElementById("settings").style.color = 'black';
 
-    defaultColor = document.getElementById("downloadText").style.color;
-    document.getElementById("downloadText").style = "color: #00000000;"
-    document.getElementById("add2down").style = "color: #00000000;"
+    // defaultColor = document.getElementById("downloadText").style.color;
+    // document.getElementById("downloadText").style = "color: #00000000;"
+    document.getElementById("add2down").style.color = "white;"
     document.getElementById("myNav").style.width = "0%";
 }
 
@@ -231,59 +249,6 @@ function slidePanel() {
         open()
     }
     panelOpen = !panelOpen;
-}
-
-function download() {
-    if (items.length > 0) {
-            
-        // Creation/writing
-        const doc = new jsPDF();
-        // Set font size and type
-        doc.setFontSize(22);
-        doc.setFont('courier', 'bold');
-
-        // Add heading to the PDF
-        doc.text("My Shopping List", 20, 20);
-
-        // Set font size and type for the date
-        doc.setFontSize(14);
-        doc.setFont('courier', 'normal');
-
-        // Get the current date and format it
-        const currentDate = new Date();
-        const dateString = currentDate.toLocaleDateString();
-
-        // Add the date to the PDF on a new line
-        doc.text(`Date: ${dateString}`, 20, 30);
-
-        doc.setFontSize(12);
-        doc.setFont('courier', 'normal');
-
-        doc.lineHeight = 0.5;
-        let startY = 45; // Initial y position
-        for (let i = 0; i < items.length; i++) {
-            const item = items[i];
-            // Check if the text will overflow to the next page
-            const pageHeight = doc.internal.pageSize.height;
-            const lineHeight = doc.getLineHeight();
-            const textHeight = lineHeight * doc.splitTextToSize(item.description, doc.internal.pageSize.width - 40).length;
-            if (startY + textHeight > pageHeight - 20) {
-                doc.addPage(); // Add a new page if the text will overflow
-                startY = 45; // Reset the y position
-            }
-            doc.text(item.description, 20, startY);
-            startY += textHeight - 5; // Add some padding between lines
-        }
-
-        // Downloading
-        doc.save("Shopping_List_" + downloadCount + ".pdf");
-        downloadCount += 1;
-    }
-
-    else {
-        document.getElementById("add2down").style.color = 'rgb(199, 64, 64)';
-        setTimeout('document.getElementById("add2down").style.color = "white"', 400)
-    }
 }
 
 let xDown = null;
@@ -326,9 +291,7 @@ function handleTouchMove(event) {
   yDown = null;
 }
 
-downloadButton.addEventListener("click", function () {
-    download();
-})
+var submitButton = document.getElementById("submitBut")
 
 addButton.addEventListener("click", function () {
     createItem();
@@ -385,3 +348,218 @@ settingsHolder.addEventListener("click", function () {
 fScreenButton.addEventListener("click", function () {
     slidePanel();
 })
+
+function isValidDetail(invoiceNumber, bsb, accountNumber, abn, email) {
+    let validFound = true;
+  
+    // Check if invoice number is numeric
+    if (!isNumeric(invoiceNumber)) {
+      validFound = false;
+      console.log("Invoice Number must be numeric.");
+    }
+
+    // Check if BSB is a 6-digit number
+    if (!isValidAccNo(bsb)) {
+      validFound = false;
+      console.log("BSB must be a 6-digit number.");
+    }
+  
+    // Check if account number is a 6-digit number
+    if (!isValidAccNo(accountNumber)) {
+      validFound = false;
+      console.log("Account Number must be a 6-digit number.");
+    }
+  
+    // Check if ABN is a 11-character string
+    if (!isValidABN(abn)) {
+      validFound = false;
+      console.log("ABN must be an 11-character string.");
+    }
+  
+    // Check if invoice number is numeric
+    if (!isValidEmail(email)) {
+        validFound = false;
+        console.log("Email not valid");
+    }
+    
+    return validFound;
+  }
+  
+function isNumeric(value) {
+    return /^\d+$/.test(value);
+}
+
+function isValidAccNo(value) {
+    return /^\d{6}$/.test(value);
+}
+
+function isValidABN(value) {
+    return /^\d{11}$/.test(value);
+}
+
+function isValidEmail(email) {
+    // Regular expression pattern for email validation
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    
+    // Test the email against the pattern
+    return emailPattern.test(email);
+}
+
+document.getElementById("invoiceForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    var clientName = document.getElementById("clientNameInput").value;
+    var clientAddress = document.getElementById("clientAddressInput").value;
+    var invoiceNumber = document.getElementById("invoiceNumberInput").value;
+    var accountName = document.getElementById("accountNameInput").value;
+    var bsb = document.getElementById("bsbInput").value;
+    var accountNumber = document.getElementById("accountNumberInput").value;
+    var abn = document.getElementById("abnInput").value;
+    var email = document.getElementById("emailInput").value;
+
+    if (!isValidDetail(invoiceNumber, bsb, accountNumber, abn, email)) {
+        return;
+    }
+
+    // Writing PDF
+    if (items.length > 0) {
+        // Creation/writing
+        const doc = new jsPDF();
+
+        // Set font size and type
+        doc.setFontSize(22);
+
+        let headline = "Invoice Number " + invoiceNumber;
+
+        // Add heading to the PDF
+        doc.text(headline, 20, 20);
+
+        // Set font size and type for the date
+        doc.setFontSize(14);
+
+        // Get the current date and format it
+        const currentDate = new Date();
+        const dateString = currentDate.toLocaleDateString();
+
+        // Add the date to the PDF on a new line
+        doc.text(`Date: ${dateString}`, 20, 30);
+
+        doc.setFontSize(15);
+        doc.text("Client Information", 20, 50);
+        doc.setFontSize(12);
+
+        // Add client information
+        doc.text("Client Name: " + clientName, 20, 60);
+        doc.text("Client Address: " + clientAddress, 20, 65);
+
+        doc.setFontSize(15);
+        doc.text("Payment Details", 20, 80);
+        doc.setFontSize(12);
+
+        // Add payment details
+        doc.text("Account Name: " + accountName, 20, 90);
+        doc.text("BSB: " + bsb, 20, 95);
+        doc.text("Account Number: " + accountNumber, 20, 100);
+        doc.text("ABN: " + abn, 20, 105);
+
+        doc.setFontSize(15);
+        doc.text("Total Due: $ " + parseFloat(total).toFixed(2), 20, 125);
+        doc.setFontSize(12);
+
+        doc.setFontSize(15);
+        doc.text("Summary", 20, 135);
+        doc.setFontSize(12);
+
+        // Table headers
+        var headers = [
+            { header: 'Description', dataKey: 'description' },
+            { header: 'Dates', dataKey: 'quantity' },
+            { header: 'Price', dataKey: 'price' },
+        ];
+
+        console.log("Tota", total)
+        console.log(items)
+
+        // Hacky solution        
+        const sumItem = new Item(' ', 'Total:', parseFloat(total).toFixed(2));
+        items.push(sumItem)
+
+        // Table data
+        var data = items.map(function(item) {
+            return {
+                description: item.description,
+                quantity: item.quantity,
+                price: "$ " + parseFloat(item.price).toFixed(2)
+            };
+        });
+
+        removeElement(null, sumItem.id, sumItem);
+        console.log(items)
+        console.log(total)
+
+        // Table options
+        var options = {
+            startY: 150,
+            margin: { top: 20 },
+            styles: {
+                font: 'courier',
+                fontSize: 12,
+                cellWidth: 60,
+                valign: 'middle',
+                halign: 'left',
+                fillColor: [255, 255, 255],
+                textColor: [0, 0, 0],
+                lineWidth: 0.5,
+                lineColor: [0, 0, 0],
+            },
+            headStyles: {
+                fillColor: [41, 128, 185],
+                textColor: [255, 255, 255],
+                fontSize: 14,
+                fontStyle: 'bold',
+            },
+            bodyStyles: {
+                textColor: [0, 0, 0],
+            },
+        };
+
+        // Generate the table
+        doc.autoTable(headers, data, options);
+
+        // Downloading
+        doc.save("Invoice " + invoiceNumber + ".pdf");
+        downloadCount += 1;
+
+        // Save the PDF as a Blob
+        const pdfBlob = doc.output('blob');
+
+        // Create a URL for the Blob
+        const pdfURL = URL.createObjectURL(pdfBlob);
+
+        // Encode the recipient email for use in the mailto URL
+        var encodedRecipientEmail = encodeURIComponent(email);
+
+        // Create the subject with the invoice number
+        var subject = 'Invoice ' + invoiceNumber;
+
+        // Encode the subject for use in the mailto URL
+        var encodedSubject = encodeURIComponent(subject);
+
+        // Create the email body
+        var body = 'Here is my latest invoice.\n(See attached)';
+
+        // Encode the body for use in the mailto URL
+        var encodedBody = encodeURIComponent(body);
+
+        // Construct the mailto URL with the encoded recipient email, encoded subject, encoded body, and PDF attachment
+        var mailtoURL = 'mailto:' + encodedRecipientEmail + '?subject=' + encodedSubject + '&body=' + encodedBody + '&attachment=' + pdfURL;
+
+        // Open the user's email client with the filled-in email fields and PDF attached
+        window.location.href = mailtoURL;
+    }
+    
+    else {
+        document.getElementById("add2down").style.color = 'rgb(199, 64, 64)';
+        setTimeout('document.getElementById("add2down").style.color = "white"', 400);
+    }
+});
